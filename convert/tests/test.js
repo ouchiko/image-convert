cc652d9fa86d6398012b1b772cea029aaee93b99
James Holden <james_holden@londonmarketing.com>
2017-12-09 23:49:42 +0000

    Fix init.sh; Changes to test; modifications to web front end

--- convert/tests/test.js
+++ convert/tests/test.js
@@ -1,9 +1,9 @@
 Convert = require("../Convert.js");
 
 try {
-    let testConvert = new Convert("test.eps");
-
-    testConvert.getEPStoPNG(1000,false);
+    let testConvert = new Convert("/app/convert/tests/test.eps", "test.eps");
+    let result = testConvert.getEPStoPNG(1000,false);
+    console.log("result: "+ result);
 } catch (exception) {
     console.log(" - Error: " + exception.message);
 }
