3a25077f1f92755a5631b0f7b89c5e9d868c14ac
James Holden <james_holden@londonmarketing.com>
2017-12-10 07:47:49 +0000

    Update: Tidying up Jade tmpl / CSS / JS for image render

--- webapp/dev/js/render_image.js
+++ webapp/dev/js/render_image.js
@@ -1,4 +1,18 @@
 window.isfound = false;
+
+function createImage(type) {
+    let container = document.createElement("div");
+    container.classList.add("imagecontainer");
+    let image = document.createElement("img");
+    image.setAttribute("src", "/assets/processed/"+window.imageid+"-"+type+".png");
+    image.classList.add("imagecontainer--item");
+    container.appendChild(image);
+    image.addEventListener("click", function() {
+        location.href = this.getAttribute("src");
+    });
+    return container;
+}
+
 window.addEventListener("load", function() {
     let wait = setInterval(function() {
         if (window.imageid) {
@@ -10,18 +24,17 @@ window.addEventListener("load", function() {
             }).then(function(data) {
                 if (data.found == 'yes') {
                     clearInterval(wait);
-                    console.log("Found, yes");
-                    let containerImage = document.querySelector(".container--image");
-                    let containerWait = document.querySelector(".container--wait");
+                    setTimeout(function(){
+                        let containerImage = document.querySelector(".container--image");
+                        let containerWait = document.querySelector(".container--wait");
 
-                    containerImage.classList.toggle("container--image__hide");
-                    containerWait.classList.toggle("container--wait__hide");
+                        containerImage.classList.toggle("container--image__hide");
+                        containerWait.classList.toggle("container--wait__hide");
 
-                    let image = document.createElement("img");
-                    image.setAttribute("src", "/assets/processed/"+window.imageid+".png");
-
-                    containerImage.appendChild(image);
-
+                        containerImage.appendChild(createImage('trans'));
+                        containerImage.appendChild(createImage('white'));
+                        containerImage.appendChild(createImage('black'));
+                    }, 500);
                 }
             })
             .catch(function(error) {
