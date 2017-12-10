5462ea2b4f88873a500e15bc76a5ce0322bd32d3
James Holden <james_holden@londonmarketing.com>
2017-12-10 17:18:09 +0000

    Limit test to one file created

--- convert/Convert.test.js
+++ convert/Convert.test.js
@@ -10,8 +10,6 @@ test('Converts EPS file to PNG with return ID', () => {
 test('Ensures we have three counts of file', () => {
     let basepath = "/app/webapp/static/assets/processed/";
     let result =
-        fs.existsSync(basepath+"8c18ecc500de49027f0ed1df1bec10b8-trans.png") &&
-        fs.existsSync(basepath+"8c18ecc500de49027f0ed1df1bec10b8-black.png") &&
-        fs.existsSync(basepath+"8c18ecc500de49027f0ed1df1bec10b8-white.png");
+        fs.existsSync(basepath+"8c18ecc500de49027f0ed1df1bec10b8-trans.png");
     expect(result).toBe(true);
 });
